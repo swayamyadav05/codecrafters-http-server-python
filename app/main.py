2784aeaac30808e553557daca24a92ad1107eb41
Mangloo69 <yadav69.swayam@gmail.com>
2024-06-04 19:09:07 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -39,12 +39,12 @@ def main():
 
                 if "echo" in path[1]:
                     string = path[1].strip("/echo/")
-                    response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}".encode
-            print(f"First par {path}")
-            # Sending the HTTP response to the client
+                    response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(string)}\r\n\r\n{string}".encode()
+                print(f"First par {path}")
 
-        print(f"Received: {data}")
-        client_socket.sendall(response)
+            # Sending the HTTP response to the client
+            print(f"Received: {data}")
+            client_socket.sendall(response)
 
 
 if __name__ == "__main__":
