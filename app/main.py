45f3e639f7f8073be471d9e5f98d7b602d07f798
Mangloo69 <yadav69.swayam@gmail.com>
2024-06-05 21:50:12 +0530

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,52 +1,38 @@
 import socket
 import threading
-from typing import Dict
+import sys
 
 
 def main():
-    print("Logs from your program will appear here!")
+    def handle_req(client, addr):
+        data = client.recv(1024).decode()
+        req = data.split("\r\n")
+        path = req[0].split(" ")[1]
+        if path == "/":
+            response = "HTTP/1.1 200 OK\r\n\r\n".encode()
+        elif path.startswith("/echo"):
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}".encode()
+        elif path.startswith("/user-agent"):
+            user_agent = req[2].split(": ")[1]
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}".encode()
+        elif path.startswith("/files"):
+            directory = sys.argv[2]
+            filename = path[7:]
+            print(directory, filename)
+            try:
+                with open(f"/{directory}/{filename}", "r") as f:
+                    body = f.read()
+                response = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(body)}\r\n\r\n{body}".encode()
+            except Exception as e:
+                response = f"HTTP/1.1 404 Not Found\r\n\r\n".encode()
+        else:
+            response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
+        client.send(response)
 
-    # Creating a server socket bound to localhost on port 4221
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-
-    server_socket.listen()
-
-    # Handling the client connection
     while True:
-        # Accepting a client connection (blocking call)
-        client_socket, client_address = server_socket.accept()
-
-        # Using 'with' to ensure the client socket is properly closed when done
-        with client_socket:
-            # Log the accepted connection
-            print(f"Accepted connection from {client_address}")
-            data = client_socket.recv(1024).decode(
-                "utf-8"
-            )  # Receiving data from the client (up to 1024 bytes)
-
-            # If no data is received, break the loop (client closed the connection)
-            if not data:
-                break
-
-            # Decode the received data and split it by HTTP line delimiter
-            request_data = data.split("\r\n")
-
-            _, path, _ = request_data[0].split()
-
-            if path == "/":
-                response = "HTTP/1.1 200 OK\r\n\r\n"
-            elif path.startswith("/echo/"):
-                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}"
-            elif path.startswith("/user-agent"):
-                user_agent = "User-Agent header not found"
-                for line in request_data:
-                    if line.startswith("User-Agent:"):
-                        user_agent = line.split(": ", 1)[1]
-                        break
-                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
-            else:
-                response = "HTTP/1.1 404 Not Found\r\n\r\n"
-            client_socket.sendall(response.encode())
+        client, addr = server_socket.accept()
+        threading.Thread(target=handle_req, args=(client, addr)).start()
 
 
 if __name__ == "__main__":
